f4e476f1cd34455ff93803f78863ce550aebbabb
dsusviela <yothebest009@gmail.com>
2019-07-04 23:15:16 -0300

    implemented, now needs to fully work with IO

--- hmac.c
+++ hmac.c
@@ -8,41 +8,56 @@ Daniel Susviela
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-  EVP_MD_CTX* mdctx = NULL;
-  const EVP_MD* md = NULL;
-  EVP_PKEY* pkey = NULL;
+// case for different errors
+void handleError() {
+}
 
-  unsigned char md_value[EVP_MAX_MD_SIZE];
-  int md_len = 0;
+int main(int argc, void **argv) {
+  if (argc != 2) {
+    printf("Cantidad de parametros invalidos \n");
+    printf("uso: ./hmac [string] [key] \n");
+    return -1;
+  }
+  // obtain parameters from I/O
+  char *textAux = (char *)argv[0];
+  char text[] = "";
+  strcpy(text, textAux);
+  int textLen = strlen(text);
+  char *kAux = (char *)argv[0];
+  char k[] = "";
+  strcpy(k, kAux);
+  int b = strlen(k);
 
-  char message[] =
-      "Now is the time for all good men to "
-      "come to the aide of their country\n";
+  // init of variables for implementation
+  EVP_MD_CTX *mdContex = NULL;
+  const EVP_MD *md = NULL;
+  EVP_PKEY *key0 = NULL;
+  unsigned char res[EVP_MAX_MD_SIZE];
+  size_t resLen = 0;
 
   OpenSSL_add_all_digests();
 
-  if (!(mdctx = EVP_MD_CTX_create()))
+  if (!(mdContex = EVP_MD_CTX_create()))
     handleError();
 
   if (!(md = EVP_get_digestbyname("SHA1")))
     handleError();
 
-  if (!(pkey = EVP_PKEY_new_mac_key(EVP_PKEY_HMAC, NULL, "password", strlen("password"))))
+  if (!(key0 = EVP_PKEY_new_mac_key(EVP_PKEY_HMAC, NULL, k, b)))
     handleError();
 
-  if (1 != EVP_DigestSignInit(mdctx, NULL, md, NULL, pkey))
+  if (1 != EVP_DigestSignInit(mdContex, NULL, md, NULL, key0))
     handleError();
 
-  /* Call update with the message */
-  if (1 != EVP_DigestSignUpdate(mdctx, message, strlen(message)))
+  /* Call update with the text */
+  if (1 != EVP_DigestSignUpdate(mdContex, text, textLen))
     handleError();
 
-  if (1 != EVP_DigestSignFinal(mdctx, md_value, &md_len))
+  if (1 != EVP_DigestSignFinal(mdContex, res, &resLen))
     handleError();
 
   printf("HMAC is: ");
-  for (int i = 0; i < md_len; i++)
-    printf("%02x", md_value[i]);
+  for (int i = 0; i < resLen; i++)
+    printf("%02x", res[i]);
   printf("\n");
 }
