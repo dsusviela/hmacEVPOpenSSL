622e91148d4a9d2234f01470d271115a3556e89f
dsusviela <yothebest009@gmail.com>
2019-07-04 23:24:25 -0300

    I/O fully implemented, code comments and readme, makefile missing

--- hmac.c
+++ hmac.c
@@ -12,28 +12,30 @@ Daniel Susviela
 void handleError() {
 }
 
-int main(int argc, void **argv) {
-  if (argc != 2) {
+int main(int argc, char **argv) {
+  if (argc != 3) {
     printf("Cantidad de parametros invalidos \n");
     printf("uso: ./hmac [string] [key] \n");
     return -1;
   }
   // obtain parameters from I/O
-  char *textAux = (char *)argv[0];
-  char text[] = "";
-  strcpy(text, textAux);
+  char *text = argv[1];
   int textLen = strlen(text);
-  char *kAux = (char *)argv[0];
-  char k[] = "";
-  strcpy(k, kAux);
+  char *k = argv[2];
   int b = strlen(k);
+
+  printf("\n");
+  printf("%s", k);
+  printf("\n");
+  printf("%s", text);
+  printf("\n");
 
   // init of variables for implementation
   EVP_MD_CTX *mdContex = NULL;
   const EVP_MD *md = NULL;
   EVP_PKEY *key0 = NULL;
   unsigned char res[EVP_MAX_MD_SIZE];
-  size_t resLen = 0;
+  int resLen = 0;
 
   OpenSSL_add_all_digests();
 
@@ -56,7 +58,7 @@ int main(int argc, void **argv) {
   if (1 != EVP_DigestSignFinal(mdContex, res, &resLen))
     handleError();
 
-  printf("HMAC is: ");
+  printf("El valor de HMAC es: ");
   for (int i = 0; i < resLen; i++)
     printf("%02x", res[i]);
   printf("\n");
